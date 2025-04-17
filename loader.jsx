cf583335edfe05eac8c164aafd8197f7fbded964
Sandhya602 <sandhyabv31@gmail.com>
2025-04-17 22:57:26 +0530

    Update loader.jsx

--- loader.jsx
+++ loader.jsx
@@ -1,1 +1,32 @@
-
+const LoadingSpinner= ({size="md" className=""}) => {
+  const sizeClasses = {
+    sm: "w-4 h-4",
+    md: "w-8 h-8",
+    lg: "w-12 h-12",
+  };
+  return (
+    <div className={'${sizeClasses[size]} ${className}'}>
+      <svg
+        className="animate-spin text-orange-600"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+          ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-BV0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962"
+          ></path>
+      </svg>
+    </div>
+    );
+};
+export default LoadingSpinner;
