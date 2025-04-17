4ddc247ea91359bfa50f06af0a5b46e6d9c7d507
Sandhya602 <sandhyabv31@gmail.com>
2025-04-17 21:17:40 +0530

    Update HomePage.jsx

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,1 +1,14 @@
-
+import Hero from "../components/hero";
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-[url('stadium.jpg')] bg-cover bg-center bg-fixed">
+      <div className="min-h-screen bg-black/40">
+        <div className="space-y-12 pd-12">
+          <div className="bg-opacity-90">
+            <Hero />
+          </div>
+        </div>
+      </div>
+   </div>
+      );
+}
