76d41b2731a6c6c1ee662856e3c78248c44a642e
Sandhya602 <sandhyabv31@gmail.com>
2025-04-17 23:40:36 +0530

    Update TeamStanding.jsx

--- TeamStanding.jsx
+++ TeamStanding.jsx
@@ -31,6 +31,67 @@ export default function TeamStanding() {
   return {
     <div className="bg-white rounded-lg shadow-md p-6">
       <h3 className="text-2xl text-bold mb-6">Points Table</h3>
+      <div className="overflow-x-auto">
+        <table className="min-w-full">
+          <thead>
+            <tr className="bg-gray-400">
+              <th className="px-4 py-3 text-center text-sm text-semibold text-gray-600">
+                Team
+              </th>
+              <th className="px-4 py-3 text-center text-sm text-semibold text-gray-600">
+                P
+              </th>
+              <th className="px-4 py-3 text-center text-sm text-semibold text-gray-600">
+                W
+              </th>
+              <th className="px-4 py-3 text-center text-sm text-semibold text-gray-600">
+                L
+              </th>
+              <th className="px-4 py-3 text-center text-sm text-semibold text-gray-600">
+                Points
+              </th>
+              <th className="px-4 py-3 text-center text-sm text-semibold text-gray-600">
+                NRR
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-100">
+            {pointsTable.map((iteam,index) => {
+               <tr 
+                 key={index}
+                 className={'${
+                   index % 2 === 0?"bg-white":"bg-gray-50"
+                 } hover:bg-gray-50 trtransition-color'}
+               >
+                 <td className"px-4 py-3">
+                   <div className="flex items-centerspace-x-3">
+                     <img
+                       src={iteam.flag}
+                       alt={iteam.team}
+                       className="w-8 h-8 rounded-full object-contains"
+                       />
+                   </div>
+                 </td>
+                 <td className="px-4 py-3 text-center text-gray-600">
+                   {iteam.played}
+                 </td>
+                 <td className="px-4 py-3 text-center text-gray-600">
+                   {iteam.won}
+                 </td>
+                 <td className="px-4 py-3 text-center text-gray-600">
+                   {iteam.loss}
+                 </td>
+                 <td className="px-4 py-3 text-center text-gray-600">
+                   {iteam.points}
+                 </td>
+                 <td className="px-4 py-3 text-center text-gray-600">
+                   {iteam.nrr}
+                 </td>
+               </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
