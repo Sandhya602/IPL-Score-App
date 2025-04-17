c94208087454d8d69a9b159a8884f7b44ca6c9ee
Sandhya602 <sandhyabv31@gmail.com>
2025-04-18 00:26:40 +0530

    Update StatsSection.jsx

--- StatsSection.jsx
+++ StatsSection.jsx
@@ -16,8 +16,26 @@ export default function StatsSection() {
           initial={{opacity:0}}
           animate={{opacity:1}}
           transition={{duration:0.5}}>
-               IPL 2025 Stats and HighLights
+          IPL 2025 Stats and HighLights
         </motion.h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {stats.map((items,index) => {
+             <motion.div
+               key={index}
+               className="bg=white/10 backdrop-blur-sm rounded-sm p-6 text-center"
+               initial={{opacity:0,y:20}}
+               animate={{opacity:1,y:20}}
+               transition={{delay:index*0.1,duration:0.5}}
+               whileHover={{y:-5,transition:{duration:0.2}}}
+               >
+               <div className="flex justify-center mb-4">
+                 <stats.icon className="h-10 w-10" />
+               </div>
+               <h3 className="text-x1 font-medium mb-2">{items.title}</h3>
+               <p className="text-3x1 font-bold">{items.value}</p>
+              </motion.div>
+           ))}
+        </div>
       </div>
     </div>
   }
